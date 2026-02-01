cbbe1959735d6f47e1bdfe5d2dd3257c24a37b21
MohammedAlaaDev <modi20272@gmail.com>
2026-02-01 16:23:22 +0200

    hero section

---
+++ frontend/src/components/custom/hero-section.tsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+
+interface ImageProps {
+    id: number;
+    documentId: string;
+    url: string;
+    alternativeText: string;
+}
+
+interface LinkProps {
+    id: number;
+    url: string;
+    label: string;
+    isExternal: boolean;
+}
+
+interface HeroSectionProps {
+    data: {
+        __component: string;
+        id: number;
+        heading: string;
+        subHeading: string;
+        image: ImageProps;
+        link: LinkProps;
+    }
+}
+
+/* 
+__component: 'layout.hero-section',
+    id: 2,
+    heading: 'epic next',
+    subHeading: 'you\'re all awesome',
+    image: {
+      id: 1,
+      documentId: 'ta87isc28qf0z3bcnh812m2j',
+      url: '/uploads/ac13_f5a971948f.jpg',
+      alternativeText: null
+    },
+  }
+
+*/
+
+const styles = {
+    header: "relative h-[600px] overflow-hidden",
+    backgroundImage: "absolute inset-0 object-cover w-full h-full",
+    overlay:
+        "relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black/50",
+    heading: "text-4xl font-bold md:text-5xl lg:text-6xl",
+    subheading: "mt-4 text-lg md:text-xl lg:text-2xl",
+    button:
+        "mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100 transition-colors",
+};
+
+
+export function HeroSection({ data }: Readonly<HeroSectionProps>) {
+    if (!data) return null;
+
+    const { heading, subHeading, link, image } = data;
+
+    console.dir(data, { depth: null });
+    return (
+        <header className={styles.header}>
+            <img
+                alt="Background"
+                className={styles.backgroundImage}
+                height={1080}
+                src={`http://localhost:1337${image.url}`}
+                style={{
+                    aspectRatio: "1920/1080",
+                    objectFit: "cover",
+                }}
+                width={1920}
+            />
+            <div className={styles.overlay}>
+                <h1 className={styles.heading}>{heading}</h1>
+                <p className={styles.subheading}>{subHeading}</p>
+                <Link className={styles.button} href={link.url}>
+                    {link.label}
+                </Link>
+            </div>
+        </header>
+    );
+}
